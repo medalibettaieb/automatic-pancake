5fa8c68a6feec82ade6cb8679eae10c8240f7bdf
= <borni>
2018-12-11 15:11:50 +0100

    Add project borni demo

---
+++ demo-borni/src/main/java/demo-borni/App.java
@@ -0,0 +1,13 @@
+package demo-borni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

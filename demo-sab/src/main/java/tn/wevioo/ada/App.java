38ba8d7c46e6e763455bcb7009f7dec5cb7ef817
tot <tot@wevioo.com>
2018-12-11 15:33:40 +0100

    add demo-sab

---
+++ demo-sab/src/main/java/tn/wevioo/ada/App.java
@@ -0,0 +1,13 @@
+package tn.wevioo.ada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

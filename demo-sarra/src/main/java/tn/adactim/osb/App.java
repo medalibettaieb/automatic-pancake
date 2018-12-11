cb80ff1cc2d523b6ad754fa20d52e6fb1846b9ea
sarra <sarra@adactim.com>
2018-12-11 14:58:58 +0100

    add the project demo-sarra

---
+++ demo-sarra/src/main/java/tn/adactim/osb/App.java
@@ -0,0 +1,13 @@
+package tn.adactim.osb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
